3aa6f62c7de16e243652b31e2ea95cf05bfa1729
PriyaSingh34 <priya955singh@gmail.com>
2023-07-24 18:23:40 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,56 @@
+
+
+import Yourtube from '../assets/projects/Yourtube.png'
+import Flashcard from '../assets/projects/Flashcard.png'
+import Weathers from '../assets/projects/weather.png'
+import  SpotifyClone from '../assets/projects/SpotifyClone.png'
+import SnakeGame from '../assets/projects/SnakeGame.png'
+
+export const data=[
+
+    {
+        id:1,
+        name:"Snake Game - Javascript",
+        image:SnakeGame,
+        github:"https://github.com/PriyaSingh34/SnakeGame",
+        live:"https://snake-game-three-azure.vercel.app/",
+    },
+    
+
+    
+    {
+        id:2,
+        name:"Spotify-CSS-Clone",
+        image:SpotifyClone,
+        github:"https://github.com/PriyaSingh34/Spotify-css-clone",
+        live:"https://spotify-css-clone.vercel.app/",
+    },
+    
+
+    {
+        id:3,
+        name:"Get Weather Report",
+        image:Weathers,
+        github:"https://github.com/PriyaSingh34/Weather-app",
+        live:"https://weather-app-theta-ruddy-27.vercel.app/",
+    },
+  
+    {
+        id:4,
+        name:"React JS Application ",
+        image:Yourtube,
+        github:"https://github.com/PriyaSingh34/GetYoutubeSubscribers",
+        live:"https://get-youtube-subscribers-three.vercel.app/",
+    },
+  
+    {
+        id:5,
+        name:"React JS Application",
+        image:Flashcard,
+        github:"https://github.com/PriyaSingh34/flashcard-generator",
+        live:"https://flashcard-generator-sable.vercel.app/",
+    },
+
+
+
+]
